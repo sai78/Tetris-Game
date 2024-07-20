bc85a706898b0abd3c9be7088ec2b3a6f8e93894
sai78 <34151355+sai78@users.noreply.github.com>
2024-07-20 19:16:43 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,1 +1,47 @@
 
+document.addEventListener("DOMContentLoaded", function () {
+    let pieceCanvas = document.getElementById("next-piece");
+    let pieceContext = pieceCanvas.getContext("2d");
+
+    let rectX = 20; 
+    let rectY = 20; 
+    let rectWidth = 50;
+    let rectHeight = 20;
+    let rectSpeed = 2; 
+
+    function drawRectangle() {
+        pieceContext.clearRect(0, 0, pieceCanvas.width, pieceCanvas.height);
+        pieceContext.fillStyle = "red";
+        pieceContext.strokeRect(rectX, rectY, rectWidth, rectHeight);
+        pieceContext.fillRect(rectX, rectY, rectWidth, rectHeight);
+        pieceContext.beginPath();
+        pieceContext.moveTo(rectX + 15, rectY);
+        pieceContext.lineTo(rectX + 15, rectY + rectHeight);
+        pieceContext.moveTo(rectX + 31, rectY);
+        pieceContext.lineTo(rectX + 31, rectY + rectHeight);
+        pieceContext.stroke();
+
+        rectY += rectSpeed;
+
+        if (rectY + rectHeight >= pieceCanvas.height) {
+            rectY = pieceCanvas.height - rectHeight;
+            return;
+        }
+
+        requestAnimationFrame(drawRectangle);
+    }
+
+    drawRectangle();
+});
+
+function handleStart() {
+  alert("clicked on Start");
+}
+
+function handleRestart() {
+  alert("clicked on Restart");
+}
+
+function handlePause() {
+  alert("clicked on pause");
+}
